d19a6c3903f96ac6b2dd3ce4fbb39a8d5dc4a013
MayMoiseeva <mmoiseeva@fordham.edu>
2019-03-05 17:54:17 -0500

    added numeration in js

--- javascript/poetry.js
+++ javascript/poetry.js
@@ -2,7 +2,20 @@ $(document).ready(function(){
 	window.onscroll=function(){
 		$(".upto").css("display","inline-block");
 		if(window.pageYOffset<100){
-		$(".upto").css("display","none");
+			$(".upto").css("display","none");
+		}
+	};
+	var numbers=$(".number");
+	for(var i=0; i<numbers.length; i++){
+		numbers[i].innerHTML=i+1;
 	}
+	var subcoll=$(".numbers");
+	for(var i=0; i<subcoll.length; i++){
+		var subc=document.getElementsByClassName("itemssubc")[i].children;
+		var first=subc[0].children[0].childNodes[1].innerText;
+		console.log(first);
+		var last=subc[subc.length-1].children[0].childNodes[1].innerText;
+		console.log(last);
+		subcoll[i].innerHTML=first+"-"+last;
 	}
 })
